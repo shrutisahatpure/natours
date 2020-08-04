841b7b66d5e7311e163999e85e2a8e934f725f59
Shruti Sahatpure <shrutisahatpure96@gmail.com>
2020-08-04 22:17:27 +0530

    mongoose err

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ dotenv.config({path : './config.env'});
 
 const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
 mongoose
-.connect(process.env.DATABASE_LOCAL,{
-//.connect(DB, {
+//.connect(process.env.DATABASE_LOCAL,{
+.connect(DB, {
     useNewUrlParser:true,
     useCreateIndex:true,
     useFindAndModify:false,
